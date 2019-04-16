baeaaee82247a3e6bb0effbc2c5fa2b6d1c9563a
KAMMATIANJALI <49675668+KAMMATIANJALI@users.noreply.github.com>
2019-04-16 21:46:40 +0530

    Add files via upload

---
+++ lab8_butter filter.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+p=float(input("enter the passband peak"))
+s=float(input("enter the stopband peak"))
+q=float(input("enter the passband freq"))
+r=float(input("enter the stopband freq"))
+T=np.arange(0,1000,10)
+l=s*s
+m=p*p
+print(l)
+print(m)
+n=float((1/l)-1)
+o=float((1/m)-1)
+w=np.log10(n/o)
+b=float(np.log10(r/q))
+A=float(w/b)
+Z=int(0.5*A)
+print(Z)
+c=float(1/(2*Z))
+d=float(o/c)
+print(d)
+D=float(q/d)
+print(D)
+e=float(2*Z)
+f=(1/(1+(T/D)**e))
+g=(f**0.5)
+plt.plot(T,g)
+
